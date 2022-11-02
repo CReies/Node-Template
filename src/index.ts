e979ec10d1250035d1fcdec82a7218ec514009f7
CReies <ccreyesmolina@gmail.com>
2022-11-02 16:06:51 -0500

    installed debug

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import 'dotenv-safe/config';
 import add from '@math/add';
+import createDebug from 'debug';
+
+const log = createDebug('Index');
+log.enabled = true;
 
 console.log(process.env.NAME);
 console.log(add(1, 2));
