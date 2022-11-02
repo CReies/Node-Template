7528e9d45748d38d6a49a4936b46441adced5e06
CReies <ccreyesmolina@gmail.com>
2022-11-02 16:11:49 -0500

    changed console.logs

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ import createDebug from 'debug';
 const log = createDebug('Index');
 log.enabled = true;
 
-console.log(process.env.NAME);
-console.log(add(1, 2));
+log(process.env.NAME);
+log(add(1, 2));
